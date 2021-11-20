13ea20d25600539b3211219f48e0a8ae022c5bd7
micmil1977 <micmil1977@gmail.com>
2021-11-20 11:47:00 +0200

    Moving JSDOM to peerDependencies and supporting older version.

--- global.js
+++ global.js
@@ -1,10 +1,28 @@
-const jsGlobals = require("jsdom/lib/jsdom/browser/js-globals.json");
+const jsdomPackage = require("jsdom/package.json");
+const oldJsdom = jsdomPackage.version.startsWith('13.');
 
-const jsGlobalEntriesToInstall = Object.entries(jsGlobals).filter(([name]) => name in global);
-
-exports.add = function (globalObject) {
+if (oldJsdom) {
+  const dom = require("jsdom/lib/jsdom/living");
+  exports.add = function (globalObject) {
+    for (const name in dom) {
+      Object.defineProperty(globalObject, name, {
+        enumerable: false,
+        configurable: true,
+        writable: true,
+        value: dom[name]
+      });
+    }
+    globalObject.Object = Object;
+    globalObject.Array = Array;
+    globalObject.eval = eval;
+  }  
+} else {
+  const jsGlobals = require("jsdom/lib/jsdom/browser/js-globals.json");
+  const jsGlobalEntriesToInstall = Object.entries(jsGlobals).filter(([name]) => name in global);
+  exports.add = function (globalObject) {
     for (const [globalName, globalPropDesc] of jsGlobalEntriesToInstall) {
       const propDesc = { ...globalPropDesc, value: global[globalName] };
       Object.defineProperty(globalObject, globalName, propDesc);
     }
+  }
 }
